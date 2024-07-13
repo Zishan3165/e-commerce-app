7800c3b1bf9b84deb156806e4be92cd145f770fa
Zishan Tareque <mzishantareque@gmail.com>
2024-07-13 09:49:01 +0600

    Fix build

---
+++ src/pages/Home copy/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProductList from '../../components/ProductList';
+import Categories from './Categories';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Categories />
+      <ProductList />
+    </>
+  );
+};
+
+export default Home;
