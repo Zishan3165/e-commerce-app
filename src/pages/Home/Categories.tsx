de8c2b9796cff38710d7f9262d5740afa828b8e7
Zishan Tareque <mzishantareque@gmail.com>
2024-07-13 13:18:27 +0600

    Improve Responsiveness

--- src/pages/Home/Categories.tsx
+++ src/pages/Home/Categories.tsx
@@ -32,7 +32,7 @@ const Categories = () => {
         </div>
         <div className="flex flex-wrap -m-4">
           {categories?.map((category) => (
-            <div className="p-4 md:w-1/3" key={category}>
+            <div className="p-4 sm:w-1/2 w-full lg:w-1/3" key={category}>
               <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
                 <div className="flex items-center mb-3">
                   <h2 className="text-gray-900 text-lg title-font font-medium">{capitalizeEachWord(category)}</h2>
